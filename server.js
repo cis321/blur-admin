2af89f4d019c362ca03da14c598a93c0682a9bad
cis321 <care_nalga01@hotmail.com>
2016-04-27 22:47:36 -0500

    add express server

---
+++ server.js
@@ -0,0 +1,25 @@
+var compression = require('compression'); //libreria para comprimir los datos cuando los envia
+var serveStatic = require('serve-static');//Manejo de los archivos de forma estatica
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var maxAge = 86400000; //Max age for caching, currently set to single day
+
+// compress all requests
+app.use(compression());
+// Serve up content from public directory
+app.use(serveStatic(__dirname + '/release', {'maxAge': maxAge}));
+//app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 9051,
+  function(){
+    console.log('I am running on port ' + (process.env.PORT || 9051) + ' biatch');
+  }
+);
